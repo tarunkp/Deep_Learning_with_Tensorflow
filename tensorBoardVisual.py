bf28841e46835f6fcee3aa7157a95cbaae009c2e
tarunkp <parihartarun@gmail.com>
2018-11-28 13:40:53 -0800

    Add displaying of DNN in notebook DNN_with_Tensorflow_1.ipynb

---
+++ tensorBoardVisual.py
@@ -0,0 +1,69 @@
+# Helper functions for TF Graph visualization
+# Taken from Alex Mordvintsev's deep dream notebook
+
+import numpy as np
+import tensorflow as tf
+from IPython.display import clear_output, Image, display, HTML
+
+def strip_consts(graph_def, max_const_size=32):
+    """Strip large constant values from graph_def."""
+    strip_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = strip_def.node.add()
+        n.MergeFrom(n0)
+        if n.op == 'Const':
+            tensor = n.attr['value'].tensor
+            size = len(tensor.tensor_content)
+            if size > max_const_size:
+                tensor.tensor_content = tf.compat.as_bytes("<stripped %d bytes>"%size)
+    return strip_def
+
+def rename_nodes(graph_def, rename_func):
+    res_def = tf.GraphDef()
+    for n0 in graph_def.node:
+        n = res_def.node.add()
+        n.MergeFrom(n0)
+        n.name = rename_func(n.name)
+        for i, s in enumerate(n.input):
+            n.input[i] = rename_func(s) if s[0]!='^' else '^'+rename_func(s[1:])
+    return res_def
+
+def show_graph(graph_def, max_const_size=32):
+    """Visualize TensorFlow graph."""
+    if hasattr(graph_def, 'as_graph_def'):
+        graph_def = graph_def.as_graph_def()
+    strip_def = strip_consts(graph_def, max_const_size=max_const_size)
+    code = """
+        <script>
+          function load() {{
+            document.getElementById("{id}").pbtxt = {data};
+          }}
+        </script>
+        <link rel="import" href="https://tensorboard.appspot.com/tf-graph-basic.build.html" onload=load()>
+        <div style="height:600px">
+          <tf-graph-basic id="{id}"></tf-graph-basic>
+        </div>
+    """.format(data=repr(str(strip_def)), id='graph'+str(np.random.rand()))
+
+    iframe = """
+        <iframe seamless style="width:800px;height:620px;border:0" srcdoc="{}"></iframe>
+    """.format(code.replace('"', '&quot;'))
+    display(HTML(iframe))
+
+# Visualizing the network graph. Be sure expand the "mixed" nodes to see their
+# internal structure. We are going to visualize "Conv2D" nodes.
+
+# Uses Examples
+#graph_def = tf.get_default_graph().as_graph_def()
+#show_graph(graph_def)
+
+#graph_def = tf.get_default_graph().as_graph_def()
+#tmp_def = rename_nodes(graph_def, lambda s:"/".join(s.split('_',1)))
+#show_graph(tmp_def)
+
+#If your graph is saved as pbtxt, you could do
+#gdef = tf.GraphDef()
+#from google.protobuf import text_format
+#text_format.Merge(open("tf_persistent.pbtxt").read(), gdef)
+#show_graph(gdef)
+
